3a7a0f0619af66b30a7ddd15cbbcceaea5e29176
JMonterrosa926 <jmontenar926@gmail.com>
2025-03-30 19:47:34 -0500

    Actualizacion

---
+++ main2.js
@@ -0,0 +1,3 @@
+// filepath: d:\PROGRAMACION WEB\GESTION DE INVENTARIO\main.js
+console.log("El archivo main.js está vinculado correctamente.");
+alert("¡Hola! El archivo JavaScript está funcionando.");
